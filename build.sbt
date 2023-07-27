fee61232bd6c46fff869ba4c9ad5b3b4195ce4a9
Tomasz Szymanski <szimano@szimano.org>
2023-07-27 09:47:10 +0200

    ox sample and logger

--- build.sbt
+++ build.sbt
@@ -2,6 +2,10 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "3.3.0"
 
+libraryDependencies += "com.softwaremill.ox" %% "core" % "0.0.9"
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.10"
+libraryDependencies += "com.typesafe.scala-logging" %% "scala-logging" % "3.9.4"
+
 lazy val root = (project in file("."))
   .settings(
     name := "ox-playground"
--- src/main/scala/org/szimano/OxPlayground.scala
+++ src/main/scala/org/szimano/OxPlayground.scala
@@ -1,7 +1,31 @@
 package org.szimano
 
+import com.typesafe.scalalogging.Logger
+
 object OxPlayground {
+  import ox.par
+
   def main(args: Array[String]): Unit = {
     println ("Szimano rules")
+
+    val log = Logger("OxPlayground")
+
+    log.info("Starting!")
+
+    def computation1: Int =
+      Thread.sleep(2000)
+      log.info("1 finished")
+      1
+
+    def computation2: String =
+      Thread.sleep(1000)
+      log.info("2 finished")
+      "2"
+
+    val result: (Int, String) = par(computation1)(computation2)
+    // (1, "2")
+    log.info("All done!")
+
+    println(result)
   }
 }
